cbd0aa0e2265cff509748cfa4b938bd68a054a61
GeorgeMa <guoyangma@gmail.com>
2020-08-17 18:20:49 +0000

    use v1 compat mode for train.py

--- train.py
+++ train.py
@@ -3,7 +3,8 @@ import sys
 import json
 import datetime
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 import multiprocessing as mp
 import argparse
 import time
